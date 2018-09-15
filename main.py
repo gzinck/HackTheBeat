1e73ba8ea909f410ebf900f77514277e53093a1e
gzinck <graeme.zinck@gmail.com>
2018-09-15 16:09:45 -0400

    Added structure for GUI loading screen

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Python main file
+from tkinter import *
+from PIL import ImageTk,Image
+from ui.launchscreen import LaunchScreen
+
+root = Tk()
+root.geometry("400x500")
+
+ls = LaunchScreen(root)
+
+root.mainloop()
---
+++ ui/launchscreen.py
@@ -0,0 +1,26 @@
+"""
+
+Created on 15 Sept 2018.
+
+@author: Graeme Zinck
+
+"""
+
+from tkinter import *
+from PIL import ImageTk,Image
+
+
+class LaunchScreen:
+    def __init__(self, root):
+        self.resizedImage = Image.open("res/gears.png").resize((60, 60))
+        self.img = ImageTk.PhotoImage(self.resizedImage)
+        self.frame = Frame(root)
+        self.label_widget = Label(self.frame,
+                    text="Analysing...",
+                    compound = CENTER)
+        self.working_widget = Label(self.frame,
+                    compound = CENTER,
+                    image=self.img)
+        self.frame.pack(expand=True)
+        self.label_widget.pack()
+        self.working_widget.pack()
